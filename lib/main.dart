bd45a852f5a3d3a27e46291c4eb76306118c4ef7
Mohamed Naser Mohamed Aldeeb <53155074+mhmaldyb510@users.noreply.github.com>
2025-01-25 18:43:54 +0200

    feat: integrate Firebase with configuration and initialization in the app

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,14 @@ import 'package:day_task/views/onboarding/sign_up_view.dart';
 import 'package:day_task/views/onboarding/splash_view.dart';
 import 'package:day_task/widgets/scaffold/main_scaffold.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const DayTask());
 }
 
